59e7c7cb4e5ccd6b71143353660c906ba8b4438d
wardy dev <wardydev@gmail.com>
2021-11-01 04:20:33 -0800

    finished the features

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,12 @@ function modalEffect() {
     const nominal = this.previousElementSibling.firstElementChild.innerHTML;
     const provider = this.previousElementSibling.firstElementChild.nextElementSibling.innerHTML;
     const harga = this.previousElementSibling.lastElementChild.innerHTML;
+    const idUser = document.querySelector('.input-id-user').value
+    const nomorHP = document.querySelector('.input-phone-number').value
 
     // console.log(provider)
 
-    changeValue(nominal, provider, harga)
+    changeValue(nominal, provider, harga, idUser, nomorHP)
     modalDetail.classList.add('active')
     bg.classList.add('active')
     
@@ -24,20 +26,30 @@ function modalEffect() {
     closeModal.addEventListener('click', function() {
         modalDetail.classList.remove('active')
         bg.classList.remove('active')
+
+        document.querySelector('.input-id-user').value = ''
+        document.querySelector('.input-phone-number').value = ''
     })
     bg.addEventListener('click', function() {
         modalDetail.classList.remove('active')
         bg.classList.remove('active')
+
+        document.querySelector('.input-id-user').value = ''
+        document.querySelector('.input-phone-number').value = ''
     })
 
 }
 
-function changeValue(nominal, provider, harga) {
+function changeValue(nominal, provider, harga, idUser, nomorHP) {
     const elNominal = document.querySelector('.nominal')
     const elProvider = document.querySelector('.provider')
     const elHarga = document.querySelector('.harga')
+    const elIdUser = document.querySelector('.id-user')
+    const elPhoneNumber = document.querySelector('.phone-number')
 
     elNominal.innerHTML = nominal
     elProvider.innerHTML = provider
     elHarga.innerHTML = harga
+    elIdUser.innerHTML = idUser
+    elPhoneNumber.innerHTML = nomorHP
 }
